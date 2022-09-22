ea543d61a8b94251176be2800ed102b918d8d913
Annie Wu <anniew@andrew.cmu.edu>
2022-09-21 22:21:11 -0400

    changed query

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -17,12 +17,12 @@ public class QueryProcessorTest {
 
     @Test
     public void knowsAboutShakespeare() throws Exception {
-        assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
+        assertThat(queryProcessor.process("hi"), containsString("bye"));
     }
 
     @Test
     public void isNotCaseSensitive() throws Exception {
-        assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
+        assertThat(queryProcessor.process("Hi"), containsString("bye"));
     }
 
 }
